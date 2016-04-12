02051899274182f7dda5a8f46f587e78f9a23316
zjnktion <zjnktion@163.com>
2016-04-12 18:26:10 +0800

    [billy]realize abstract server class.

--- billy-core/src/main/java/cn/zjnktion/billy/processor/Processor.java
+++ billy-core/src/main/java/cn/zjnktion/billy/processor/Processor.java
@@ -1,7 +1,24 @@
 package cn.zjnktion.billy.processor;
 
+import cn.zjnktion.billy.session.Session;
+import cn.zjnktion.billy.task.WriteTask;
+
 /**
  * Created by zhengjn on 2016/4/11.
  */
-public interface Processor {
+public interface Processor<S extends Session> {
+
+    void add(S session);
+
+    void remove(S session);
+
+    void write(S session, WriteTask writeTask);
+
+    void flush(S session);
+
+    void dispose();
+
+    boolean isDisposing();
+
+    boolean isDisposed();
 }
